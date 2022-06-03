702d2061d59e92c29eedab2eb3b50847ff7fdb9b
Jae Ihn <jaeihn@Jaes-MacBook-Pro.local>
2022-06-03 13:03:29 +0900

    Upload 2015 Day 8 solutions.

---
+++ 2015/day8.py
@@ -0,0 +1,20 @@
+# 2015 - DAY 8 - MATCHSTICKS
+# https://adventofcode.com/2015/day/8
+
+import os
+import regex
+
+with open(os.path.dirname(__file__)+ '/inputs/day8.txt', 'r') as f:
+     lines = f.read().split()
+f.close()
+
+# Part 1
+code = sum([len(x) for x in lines])
+memory = sum([len(x.encode("utf-8").decode('unicode-escape'))-2 for x in lines])
+
+print(code, memory, code-memory)
+
+# Part 2
+extended = sum([len(regex.sub('"', '"\"', repr(x))) for x in lines])
+
+print(extended - code)
